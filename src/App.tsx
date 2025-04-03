3f420fa276e7ebbdaec7175f770e0dda58e3982c
kow111 <chome710@gmail.com>
2025-04-03 12:56:43 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <>
+      <Button type="primary">Primary Button</Button>
+      <Button className="ms-5">asd</Button>
+    </>
+  );
+}
+
+export default App;
