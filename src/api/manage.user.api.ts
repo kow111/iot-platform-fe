883c79fc73caaeea9cb7966b213a80b15efddb74
kow111 <chome710@gmail.com>
2025-04-04 18:20:33 +0700

    view table user

--- src/api/manage.user.api.ts
+++ src/api/manage.user.api.ts
@@ -1,8 +1,34 @@
 import { axiosPrivate } from "./Axios";
 
+interface IRole {
+  id: number;
+  name: string;
+}
+
+export interface IUser {
+  id: string;
+  userName: string;
+  email: string;
+  displayName: string;
+  avatarUrl: string;
+  lastLogin: string;
+  createdAt: string;
+  updatedAt: string;
+  roles: IRole[];
+}
+
+interface IPaginationUsers {
+  totalPages: number;
+  currentPage: number;
+  users: IUser[];
+  totalElements: number;
+}
+
 export const GetAllUsersAPI = async (params: {
   page?: number;
   limit?: number;
 }) => {
-  return axiosPrivate.get<IBackendResponse<any[]>>("/users", { params });
+  return axiosPrivate.get<IBackendResponse<IPaginationUsers>>("/users", {
+    params,
+  });
 };
--- src/components/admin/user/user.table.tsx
+++ src/components/admin/user/user.table.tsx
@@ -1,42 +1,88 @@
-import { Table } from "antd";
+import { Table, TableProps } from "antd";
+import { GetAllUsersAPI, IUser } from "../../../api/manage.user.api";
+import { useEffect, useState } from "react";
 
 const UserTable = () => {
-  const dataSource = [
+  const [dataSource, setDataSource] = useState<IUser[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+    total: 0,
+  });
+  const fetchDataUser = async () => {
+    setLoading(true);
+    try {
+      const response = await GetAllUsersAPI({
+        page: pagination.current - 1,
+        limit: pagination.pageSize,
+      });
+      if (response.status === 200) {
+        setDataSource(response.data.data?.users || []);
+        setPagination({
+          ...pagination,
+          total: response.data.data?.totalElements || 0,
+        });
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      // Handle error (e.g., show a notification)
+    } finally {
+      setLoading(false);
+    }
+  };
+  const columns: TableProps<IUser>["columns"] = [
     {
-      key: "1",
-      name: "Mike",
-      age: 32,
-      address: "10 Downing Street",
+      title: "Id",
+      dataIndex: "id",
+      key: "id",
     },
     {
-      key: "2",
-      name: "John",
-      age: 42,
-      address: "10 Downing Street",
+      title: "UserName",
+      dataIndex: "userName",
+      key: "userName",
+    },
+    {
+      title: "Avatar",
+      dataIndex: "avatarUrl",
+      key: "avatarUrl",
+      render: (avatarUrl: string) => (
+        <img
+          src={avatarUrl}
+          alt="Avatar"
+          style={{
+            width: 50,
+            height: 50,
+            borderRadius: "50%",
+            objectFit: "scale-down",
+          }}
+        />
+      ),
     },
   ];
-
-  const columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-    },
-    {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
-    },
-    {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
-    },
-  ];
-
+  const handleTableChange = (pagination: any) => {
+    setPagination({
+      ...pagination,
+    });
+  };
+  useEffect(() => {
+    fetchDataUser();
+  }, [pagination.current, pagination.pageSize]);
   return (
     <div>
-      <Table dataSource={dataSource} columns={columns} />;
+      <Table
+        dataSource={dataSource}
+        loading={loading}
+        columns={columns}
+        pagination={{
+          current: pagination.current,
+          pageSize: pagination.pageSize,
+          total: pagination.total,
+        }}
+        rowKey={(record) => record.id}
+        onChange={handleTableChange}
+      />
+      ;
     </div>
   );
 };
