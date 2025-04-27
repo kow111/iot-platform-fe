fdf6cc7591ba99acef6eadc4fd1972a7f59283b2
DucPhu2308 <nguyenducphu2308@gmail.com>
2025-04-27 21:09:02 +0700

    update, delete device

---
+++ src/api/device-attribute-value.ts
@@ -0,0 +1,42 @@
+import { axiosPrivate } from "./Axios";
+import { IDeviceAttribute } from "./device-attribute.api";
+import { IDevice } from "./device.api";
+
+export interface IDeviceAttributeValue {
+    id: string;
+    deviceId: IDevice;
+    deviceAttributeId: IDeviceAttribute;
+    value: string;
+}
+
+export interface ICreateDeviceAttributeValue {
+    deviceId: string;
+    deviceAttributeId: string;
+    value: string;
+}
+
+export class DeviceAttributeValueApi {
+    static getAllDeviceAttributeValues(deviceId: string) {
+        return axiosPrivate.get<IBackendResponseMessage<IDeviceAttributeValue[]>>(
+            "/device-attribute-values",
+            { params: { deviceId } }
+        );
+    }
+
+    static createDeviceAttributeValue(data: ICreateDeviceAttributeValue) {
+        return axiosPrivate.post<IBackendResponseMessage<IDeviceAttributeValue>>(
+            "/device-attribute-value",
+            data
+        );
+    }
+
+    static updateDeviceAttributeValue(
+        id: string,
+        data: ICreateDeviceAttributeValue
+    ) {
+        return axiosPrivate.put<IBackendResponseMessage<IDeviceAttributeValue>>(
+            `/device-attribute-value/${id}`,
+            data
+        );
+    }
+}
---
+++ src/components/user/room-details/attribute-values.modal.tsx
@@ -0,0 +1,12 @@
+import { Checkbox, Col, Form, FormProps, Input, Modal, Row, Select } from "antd";
+
+const AttributeValuesModal = () => {
+    return (
+        <div>
+        <h2>Attribute Values Modal</h2>
+        {/* Add your modal content here */}
+        </div>
+    );
+};
+
+export default AttributeValuesModal;
